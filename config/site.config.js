10d21ed8e65d7d93d15b239bab10dfadc0daaa96
aboyzy <2663221849@qq.com>
2023-06-24 15:07:46 +0800

    fix

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/盘2/sese'],
+  protectedRoutes: ['/盘2/原博客内容'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:zhi@aboyzy.top',
